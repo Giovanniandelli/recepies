90f9bbf40094a3a7fbc220aa62aabea2876b9392
Giovanni Iandelli <Giovanni@MacBook-di-Francesco.local>
2019-05-08 10:07:58 +0200

    prova finale

--- js/controllers/recipie.js
+++ js/controllers/recipie.js
@@ -1,16 +1,82 @@
-function loadRecepie(){
+var db = firebase.firestore();
 
-}
+function loadRecepie(){
+    var recepieId = GetIdFromUrl();
+
+    db.collection('recepies').doc(recepieId).onSnapshot(function(item){
+         var recepie = item.data();
+         var recepieId = GetIdFromUrl();
+          addRecepie(recepie,recepieId);
+         // console.log(recepieIdFromDb);
+         // console.log(recepieElementFromDb);
+        } );
+      
+      };
+
+
+
 
 function GetIdFromUrl(){
-
+    var UrlID = window.location.search;
+    var UrlID2 = UrlID.substring(4, 10000);
+    //console.log(UrlID);
+   // console.log(UrlID2);
+    return(UrlID2);
 }
 
 
-function findGetParameter(parameterName){
-    var result = null;
-        tmp = [];
-    location.search
-        .substr
-}
+
+
 loadRecepie(); 
+
+function addRecepie(recepie, recepieId){
+    var $sourceElement = $('recepie-source-container');
+    var title = recepie.name;
+    $('#NomeRicetta').text(title);
+    console.log(title);
+
+    var nPersone = recepie.people;
+    $('#NumeroPersone').text(nPersone);
+
+    var tpreparazione = recepie.preparationTime;
+    var Tpreparazione = tpreparazione.concat(" min");
+    $('#TempoPreparazione').text(Tpreparazione);
+
+    var tcottura = recepie.cookingTime;
+    var Tcottura = tcottura.concat(" min");
+    $('#TempoCottura').text(Tcottura);
+
+    var Costo = recepie.cost;
+    $('#costo').text(Costo)
+
+    var Descrizione = recepie.description;
+    $('#Descrizione').text(Descrizione);
+
+    var Diff = recepie.difficulty;
+    console.log(Diff);
+    switch (Diff) {
+        case 0:
+        $('#0stelle').css("display", "block");
+        break;
+        case 1:
+        $('#1stelle').css("display", "block");
+        break;
+        case "2":
+        $('#2stelle').css("display", "block");
+        break;
+        case "3":
+        $('#3stelle').css("display", "block");
+        break;
+        case "4":
+        $('#4stelle').css("display", "block");
+        break;
+        case "5":
+        $('#5stelle').css("display", "block");
+        break;
+    }
+}
+
+addRecepie();
+function clearContainer(){
+    $('#recepies-list-container').empty();
+  }
